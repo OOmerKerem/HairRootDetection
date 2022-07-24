cdb0c70ad0c0b3690cfe990b00b7930d81d8e038
Omerettrki <57484187+Omerettrki@users.noreply.github.com>
2022-07-24 21:20:14 +0300

    Main Code
    Comments added

--- HairRootDetection.py
+++ HairRootDetection.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2
 
-def nothing():
+def nothing(): ## boş fonksiyon
     pass
 
-def DrawHairs (img, hairs):
+def DrawHairs (img, hairs): # Saç köklerini çizen fonksiyon
     img = np.copy(img)
     blank_image = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
 
@@ -15,31 +15,30 @@ def DrawHairs (img, hairs):
     img = cv2.addWeighted(img, 0.8, blank_image, 1, 0.0)
     return img
 
-cv2.namedWindow("Contrast-Brightness")
-cv2.createTrackbar("Alpha", "Contrast-Brightness", 47, 300, nothing)
-cv2.createTrackbar("Beta", "Contrast-Brightness", 73, 300, nothing)
+## Arayüz penceresi
+cv2.namedWindow("Canny Threshold")
+cv2.createTrackbar("Lower", "Contrast-Brightness", 47, 300, nothing)
+cv2.createTrackbar("Upper", "Contrast-Brightness", 73, 300, nothing)
+##
 
+path = r"C:\Users\Kerem Ozben\Documents\GitHub\HairRootDetection\Samples\Sample1.jpeg" ## Fotoğrafın olduğu path
+img = cv2.imread(path) ## fotoğrafı okuma
 
-#alpha = float(input('* Enter the alpha value [1.0-3.0]: '))
-#beta = int(input('* Enter the beta value [0-100]: '))
-
-path = r"C:\Users\Kerem Ozben\Documents\GitHub\HairRootDetection\Samples\Sample1.jpeg"
-img = cv2.imread(path)
 while True:
-    alpha = cv2.getTrackbarPos("Alpha", "Contrast-Brightness")
-    beta = cv2.getTrackbarPos("Beta", "Contrast-Brightness")
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+    alpha = cv2.getTrackbarPos("Lower", "Contrast-Brightness") ## arayüzden değer okuma
+    beta = cv2.getTrackbarPos("Upper", "Contrast-Brightness")
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # grayscale geçme
+    blurred = cv2.GaussianBlur(gray, (7, 7), 0) # blur uygulama
 
-    #cont_bright = cv2.convertScaleAbs(gray, alpha = alpha, beta = beta)
+    #cont_bright = cv2.convertScaleAbs(gray, alpha = alpha, beta = beta) # parlaklık kontrast ayarlama
     #gray = cv2.cvtColor(cont_bright, cv2.COLOR_BGR2GRAY)
 
 
-    canny = cv2.Canny(blurred, alpha, beta)
-    hairs = cv2.HoughLinesP(canny, 1, np.pi/180, 0)
+    canny = cv2.Canny(blurred, alpha, beta) # Canny kenar tespiti
+    hairs = cv2.HoughLinesP(canny, 1, np.pi/180, 0) # Hough çizgi tespiti
     print(hairs)
 
-    img_withHairs = DrawHairs(img, hairs)
+    img_withHairs = DrawHairs(img, hairs) # Fonksiyon ile çizgiler çizilir.
 
 
     cv2.imshow("image", img)
